a7abb579a1c5ae8e536ae88356d5c26c336ad4a8
Ankit Aggarwal <ankit.agg.2008@gmail.com>
2024-04-05 14:26:08 -0400

    Delete Weather_API Key
    Delete Weather_API Key

--- scripts/weather-api-example/weather_key.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-#these are client keys to call a s3 bucket and weather api, use import weather_key after saving this to your Lib directory
-aws_access_key_id = 'AKIAVRUVSNLX5LCIHAQQ'
-aws_secret_access_key = 'dzuxJAHOdYtOyOvwckuIFNYh0xRkur0iKJNUO4l4'
-api_key = '3395c709b27245ebf061a4f13be44ef1'
